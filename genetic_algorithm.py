9b9cb02a3df420c2b8c282b9eee6c67658a4c172
Dimitris Stylianou <dimitris.volley1911@gmail.com>
2023-05-07 14:11:31 +0300

    mutation and elitism method added.

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -56,12 +56,11 @@ def partial_population_renewal():
     """
     Partial population renewal.
     """
-    number_of_renewed_solutions = int(N*0.3) # Number of solutions that will be replaced by the new generation.
+    number_of_renewed_solutions = int(N*0.6) # Number of solutions that will be replaced by the new generation.
     if number_of_renewed_solutions % 2 != 0:
         number_of_renewed_solutions += 1
     
-    directly_passed_solutions = N - number_of_renewed_solutions # Number of solutions that will be directly passed to the next generation.
-    return number_of_renewed_solutions, directly_passed_solutions
+    return number_of_renewed_solutions
 
 def tournament_selection(fitness_list, number_of_renewed_solutions):
     """
@@ -86,7 +85,7 @@ def tournament_selection(fitness_list, number_of_renewed_solutions):
                     cpd.pop(i)
                     return select_a_competitor()
                         
-    for _ in range(number_of_renewed_solutions*2):
+    for _ in range(number_of_renewed_solutions):
         competitors = rnd.sample(range(len(fitness_list)), tournament_size)
         fitnesses = [fitness_list[competitor] for competitor in competitors]
         relative_fitnesses = [fitness/sum_fitness for fitness in fitnesses]
@@ -119,15 +118,66 @@ def one_point_crossover(solutions, mating_pool):
         new_solutions.append(solutions[pair[1]][:crossover_point] + solutions[pair[0]][crossover_point:])
     return new_solutions
 
+def mutation(solutions, fitness_list):
+    """
+    Mutation is applied to the solutions with the lowest fitness values.
+    """
+    number_of_mutations = int(N*0.1) # Number of solutions that will be mutated.
+    fl = fitness_list.copy()
+    mutated_solutions = []
+    mutated_solutions_indexes = []
+    
+    while number_of_mutations > 0:
+        min_fitness_idx = fl.index(min(fl))
+        fl.pop(min_fitness_idx)
+        solution = solutions[min_fitness_idx]
+        rnd_idx = rnd.randint(0, 15)
+        rnd_color = rnd.choice(colors)
+        solution = solution[:rnd_idx] + rnd_color + solution[rnd_idx+1:]
+        mutated_solutions.append(solution)
+        mutated_solutions_indexes.append(min_fitness_idx)
+        number_of_mutations -= 1
+        
+    return mutated_solutions, mutated_solutions_indexes
+
+def elitism(solutions, fitness_list):
+    """
+    Find the solution with the highest fitness value and return it as the elite solution.
+    """
+    elite_solution = solutions[fitness_list.index(max(fitness_list))]
+    elite_solution_index = fitness_list.index(max(fitness_list))
+
+def choose_the_remaining_solutions(solutions, crossover_len, mutated_solutions_indexes, elite_solution_index):
+    """
+    Choose the remaining solutions from the initial solutions. This implementation
+    is choosing the solutions that are not selected for mutation or elitism.
+    """
+    remaining_solutions_indexes = []
+    mutation_len = len(mutated_solutions_indexes)
+    size_of_remaining_solutions = N - crossover_len - mutation_len - 1 # -1 for the elite solution.
+    
+    while size_of_remaining_solutions > 0:
+        rnd_idx = rnd.randint(0, N-1)
+        while rnd_idx in mutated_solutions_indexes or rnd_idx == elite_solution_index or rnd_idx in remaining_solutions_indexes:
+            rnd_idx = rnd.randint(0, N-1)
+        remaining_solutions_indexes.append(rnd_idx)
+        size_of_remaining_solutions -= 1
+        
+    return [solutions[idx] for idx in remaining_solutions_indexes]
+
+
 if __name__ == '__main__':
     colors = ['B', 'R', 'G', 'Y'] # Blue, Red, Green, Yellow
-    N = 20 # Number of initial solutions
+    N = 6 # Number of initial solutions
     graph_dict = import_graph()
     initial_solutions = generate_initial_solutions()
+    number_of_renewed_solutions = partial_population_renewal()
+    
     fitness_list = fitness_function(initial_solutions)
-    number_of_renewed_solutions, directly_passed_solutions = partial_population_renewal()
     mating_pool = tournament_selection(fitness_list, number_of_renewed_solutions)
-    #print(f'Number of renewed solutions: {number_of_renewed_solutions}')
-    #print(mating_pool)
-    new_solutions = one_point_crossover(initial_solutions, mating_pool)
+    print(f'Number of renewed solutions: {number_of_renewed_solutions}')
+    crossovered_solutions = one_point_crossover(initial_solutions, mating_pool)
+    mutated_solutions, mutated_solutions_indexes = mutation(initial_solutions, fitness_list)
+    elite_solution, elite_solution_index = elitism(initial_solutions, fitness_list)
+    remaining_solutions = choose_the_remaining_solutions(initial_solutions, len(crossovered_solutions), mutated_solutions_indexes, elite_solution_index)
     
